371512047bce78c41f63ef9d5c94caf52725cee7
本川　晴彦 <harx6009@icloud.com>
2025-01-03 13:05:33 +0900

    feat: シードカラー変更

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -2,5 +2,5 @@ import 'dart:ui';
 
 class Constants {
   static const appName = 'AsyncValue with switch Sample';
-  static const baseSeedColor = Color.fromARGB(255, 98, 0, 255);
+  static const baseSeedColor = Color.fromARGB(255, 16, 124, 240);
 }
